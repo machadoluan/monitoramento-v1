25c84ae978370cfffee38c2eaac1c96ade60a183
machadoluan <machado.luandealmeida@gmail.com>
2025-06-15 18:37:21 -0300

    Configura CORS no bootstrap para permitir origens específicas e métodos HTTP adequados.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: ['https://pwm.devmchd.space', 'http://localhost:4200'],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
